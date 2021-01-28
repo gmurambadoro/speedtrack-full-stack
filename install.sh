dc19347a0a1145c0ed46a0fd57cfec366ab123b7
gmurambadoro <developer.gavin@gmail.com>
2021-01-28 15:56:20 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -35,27 +35,35 @@ fi
 # navigate into webroot and create installation folders
 cd $WEB_DIR || exit 1
 
-# check
+# attempt to create the target directors and silently fail if they already exist
 sudo mkdir "$FRONTEND_DIR" "$BACKEND_DIR"
 
-# check that the required directories exist
+# check that the required directories exist and we can access their contents
 ls -ah "$FRONTEND_DIR" || exit 1
 ls -ah "$BACKEND_DIR" || exit 1
 
+# set proper permissions recursively for apache user
 sudo chown -R www-data:www-data "$FRONTEND_DIR" "$BACKEND_DIR"
 sudo chown -R www-data:www-data "$FRONTEND_DIR" "$BACKEND_DIR"
 
+# the tmp directory will be the working folder
 cd /var/tmp/ || exit 1
 
-# remove any existing folder is available
+# delete the working folder if it already exists
 rm -rf speedtrack-full-stack
 git clone https://github.com/gmurambadoro/speedtrack-full-stack.git speedtrack-full-stack
 cd speedtrack-full-stack || exit 1
+echo
+echo ".env" => BEFORE
 cat .env
 echo
 echo "REACT_APP_API_BASE_URL=${BACKEND_URL}" > .env
 echo
+echo echo ".env" => AFTER
 cat .env
+echo
+
+echo "!!=== BUILDING THE FRONTEND ===!!"
 echo
 
 npm install
@@ -63,6 +71,11 @@ npm run build
 sudo rsync -avzhr build/ "${WEB_DIR}/${FRONTEND_DIR}/" || exit 1
 
 cd backend || exit 1
+
+echo
+echo
+echo "!!=== BUILDING THE BACKEND ===!!"
+echo
 npm install
 cd ../
 sudo rsync -avzhr backend/ "${WEB_DIR}/${BACKEND_DIR}/" || exit 1
